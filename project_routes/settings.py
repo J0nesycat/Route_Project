90918d24255dc3692385b3283191436274dc2b3e
J0nesycat <e9_9k@protonmail.com>
2025-04-15 20:59:38 +0300

    Fixies

--- project_routes/settings.py
+++ project_routes/settings.py
@@ -26,13 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6+@b6b_!x1_mpyfnh9-tw1put8_@s%+7t1z3z+9(#5f^rle7cw'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
